600d7904433e725f28c1f244412510def8280486
mrublev <41304297+rublevmartin@users.noreply.github.com>
2024-10-14 22:42:10 +0300

    Added Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderNav />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderNav from './components/HeaderNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderNav
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,83 @@
+<template>
+  <header>
+    <nav class="nav">
+      <div class="nav__side">
+        <a href="#" class="nav__back">
+          <img src="arrow-left.svg" alt="Back">
+        </a>
+
+        <a href="#" class="nav__logo">
+          <img src="logo.svg" alt="Oddspedia Logo">
+        </a>
+      </div>
+
+      <div class="nav__side">
+        <a href="#" class="nav__alert">
+          <img src="notification.svg" alt="Notification Icon">
+
+          <span class="nav__alert-index">2</span>
+        </a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 80px;
+  padding: 10px 32px;
+  background-color: $dark-color;
+
+  a {
+    display: inline-block;
+    transition: transform .2s;
+
+    &+a {
+      margin-left: 15px;
+    }
+
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
+
+  &__side {
+    line-height: 1;
+  }
+
+  &__alert {
+    position: relative;
+
+    &:hover {
+      .nav__alert-index {
+        transform: translate(5px, 5px) scale(1.1);
+      }
+    }
+
+    &-index {
+      $nav-alert-border: 2px;
+      $nav-alert-radius: 9px;
+      $nav-alert-diameter: $nav-alert-radius*2;
+      $nav-alert-line-height: $nav-alert-diameter - $nav-alert-border*2;
+
+      position: absolute;
+      bottom: -5px;
+      right: -$nav-alert-radius;
+      display: inline-block;
+      width: $nav-alert-diameter;
+      height: $nav-alert-diameter;
+      border-radius: $nav-alert-radius;
+      color: #fff;
+      line-height: $nav-alert-line-height;
+      text-align: center;
+      font-size: $nav-alert-radius + 1;
+      border: $nav-alert-border solid $dark-color;
+      background-color: $accent-color;
+      transition: transform .3s;
+    }
+  }
+}
+</style>
