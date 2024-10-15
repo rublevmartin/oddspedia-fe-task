83088450586a656eb8c2acc025a34a73c1ae1cc2
mrublev <41304297+rublevmartin@users.noreply.github.com>
2024-10-15 22:56:32 +0300

    Added search field

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -5,7 +5,25 @@
     </div>
 
     <div class="container-body">
+      <section class="section">
+        <div class="section__head">
+          <h2 class="section__title">
+            Search Teams
+          </h2>
+        </div>
 
+        <div class="section__body">
+          <input type="text" class="field">
+        </div>
+      </section>
+
+      <section class="section">
+        <div class="section__head">
+          <h2 class="section__title">
+            My Teams
+          </h2>
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -19,3 +37,37 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+.field {
+  width: calc(100% - 30px);
+  height: 36px;
+  line-height: 36px;
+  border-radius: 18px;
+  padding: 0 36px;
+  margin: 0 15px 6px;
+  border: 0;
+  background-color: $base-color;
+}
+
+.container-body {
+  background-color: $base-darker-color;
+}
+
+.section {
+  background-color: $white-color;
+  color: $title-color;
+
+  &+.section {
+    margin-top: 8px;
+  }
+
+  &__head {
+    padding: 16px 15px;
+  }
+
+  &__body {
+    padding: 0 0 10px;
+  }
+}
+</style>
